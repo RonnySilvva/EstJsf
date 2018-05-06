46809a4d9582fb2ae8ba82651ca96168779ae74f
RonnySilvva <roniel.henrique@hotmail.com>
2018-05-06 15:01:49 -0300

    Commit JSF

---
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -0,0 +1,39 @@
+package br.com.cursojsf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ViewScoped
+@ManagedBean(name = "pessoaBean")
+
+public class PessoaBean {
+
+	private String nome;
+
+	List<String> nomes = new ArrayList<String>();
+	
+	public void addNome(){
+		nomes.add(nome);
+	}
+	
+	public List<String> getNomes() {
+		return nomes;
+	}
+	
+	public void setNomes(List<String> nomes) {
+		this.nomes = nomes;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+}
